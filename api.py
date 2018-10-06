690237ef7f7ec232e10aebfb0401c6c6450d282c
Cnels1986 <CNels1986@gmail.com>
2018-10-06 16:49:24 -0400

    delete function works, as does the list all games and list a game by its id. Output still needs to be put into JSON

--- api.py
+++ api.py
@@ -1,20 +1,48 @@
 from flask import Flask
+from flaskext.mysql import MySQL
+from flask import jsonify
+
 app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'apidb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
 
 @app.route('/')
 def home():
     return 'Home page'
 
+
 @app.route('/games')
 def index():
-    return 'Games page'
+    cursor.execute('SELECT * FROM games')
+    test = cursor.fetchall()
+    print(test)
+
+    return "whatever"
+    # return jsonify(id=g.test.id,
+    #                name=g.test.name,
+    #                year=g.test.year,
+    #                console=g.test.console)
 
 @app.route('/games/<int:game_id>')
 def show(game_id):
+    cursor.execute('SELECT * FROM games WHERE id = %d' % game_id)
+    test = cursor.fetchone()
+    print(test)
     return 'Show game %d' % game_id
 
 @app.route('/delete/<int:game_id>')
 def delete(game_id):
+    cursor.execute('DELETE FROM games WHERE id = %d' % game_id)
     return 'Delete %d' % game_id
 
 @app.route('/update/<int:game_id>')
